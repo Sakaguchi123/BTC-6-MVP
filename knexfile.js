01ac15a62d1562ff203b3472f9057ece1fd69d15
Sakaguchi123 <chiyumi_sakaguchi@mail.toyota.co.jp>
2024-05-24 21:27:51 +0900

    fix : knexfile:production編集

--- knexfile.js
+++ knexfile.js
@@ -20,8 +20,8 @@ module.exports = {
     client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: "./data/migrations",
+      directory: "./db/migrations",
     },
-    seeds: { directory: "./data/seeds" },
+    seeds: { directory: "./db/seeds" },
   },
 };
