dc9db74d56f5de8042538cc800f65810f749d1e5
Sakaguchi123 <chiyumi_sakaguchi@mail.toyota.co.jp>
2024-05-27 19:54:43 +0900

    test : renderテスト2

--- knexfile.js
+++ knexfile.js
@@ -20,7 +20,7 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: 'db/migrations'
+      directory: './db/migrations'
     },
     seeds: { directory: './db/seeds' }
   }
